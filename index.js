f5b8fbc6ef05f245e4f76101aafd925c8a207b14
aguerram <agurram2013@gmail.com>
2020-11-25 17:27:17 +0100

    fix bug

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 module.exports = templateRender = (str, data) => {
-  if (typeof str !== "string") throw "Input musr be a string";
-  if (typeof data !== "object") throw "Data musr be an object";
+  if (typeof str !== "string") throw "Input must be a string";
+  if (typeof data !== "object") throw "Data must be an object";
 
-  return str.replace(/\{([a-z]+){1}\}/g, (a, b) => {
+  return str.replace(/\{([a-z_]+[a-zA-Z0-9_]+){1}\}/g, (a, b) => {
     return data[b] ? data[b] : b;
   });
 };
